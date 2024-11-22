8de222d7f9f727089aac6ae977ab3262ce469d83
samreis007 <samtosreis2121@gmail.com>
2024-11-22 12:11:42 -0300

    Update test_calculadora.py

--- test_calculadora.py
+++ test_calculadora.py
@@ -13,14 +13,24 @@ class TestCalculadora(unittest.TestCase):
         self.calc = Calculadora()
 
         # 5) criando o teste
-        # do método soma
+        # do método "soma"
     def test_soma(self):
             self.assertEqual(self.calc.soma(10, 10), 20, "deve somar dois números")
         
         # 6) criando o teste
-        # do método soma
+        # do método "subtracao"
     def test_subtracao(self):
             self.assertEqual(self.calc.subtracao(10, 10), 0, "deve subtrair dois números")
+
+        # 7) criando o teste
+        # do método "multiplicacao"
+    def test_subtracao(self):
+            self.assertEqual(self.calc.multiplicacao(10, 10), 100, "deve multiplicar dois números")
+
+        # 8) criando o teste
+        # do método "divisao"
+    def test_subtracao(self):
+            self.assertEqual(self.calc.divisao(10, 10), 1, "deve dividir dois números")
 
 # executar a classe de testes unitários
 if __name__ == "__main__":
