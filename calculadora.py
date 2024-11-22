dde0fb37a91f2b72da01fdc376125c924242edbd
samreis007 <samtosreis2121@gmail.com>
2024-11-22 12:05:52 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,9 @@ class Calculadora:
     
     def subtracao(self, n1, n2):
         return n1 - n2
+
+    def multiplicacao(self, n1, n2):
+        return n1 * n2
+
+    def divisao(self, n1, n2):
+        return n1 / n2
